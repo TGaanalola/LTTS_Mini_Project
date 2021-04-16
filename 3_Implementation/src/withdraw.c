9eeb671bb20b50a439b5e9825d12d9b02219220a
TGaanalola <81503646+TGaanalola@users.noreply.github.com>
2021-04-16 14:11:22 +0530

    Update withdraw.c

--- 3_Implementation/src/withdraw.c
+++ 3_Implementation/src/withdraw.c
@@ -1,1 +1,18 @@
-
+#include<stdio.h>
+#include "bank.h"
+void withdraw(customer list[], int s, int num, int amt)
+{
+    int i = search(list, s, num);
+    if (i ==  - 1)
+    {
+        printf("Record not found\n");
+    } 
+    else if (list[i].balance < amt)
+    {
+        printf("Insufficient balance\n");
+    }
+    else
+    {
+        list[i].balance -= amt;
+    } 
+}
