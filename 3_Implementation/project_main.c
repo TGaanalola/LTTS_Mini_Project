762e3dae7d5102b692449db6750c61514c91d668
TGaanalola <81503646+TGaanalola@users.noreply.github.com>
2021-04-16 13:17:02 +0530

    Update project_main.c

--- 3_Implementation/project_main.c
+++ 3_Implementation/project_main.c
@@ -1,112 +1,74 @@
-#include <calculator_operations.h>
+#include <stdio.h>
+#include<string.h>
+#include "bank.h"
 
-/* Status of the operation requested */
-#define VALID   (1)
-#define INVALID (0)
-
-/* Calculator operation requested by user*/
-unsigned int calculator_operation = 0;
-
-/* Operands on which calculation is performed */
-int calculator_operand1 = 0;
-int calculator_operand2 = 0;
-
-/* Valid operations */
-enum operations{ ADD=1, SUBTRACT, MULTIPLY, DIVIDE, EXIT };
-
-/* Display the menu of operations supported */
-void calculator_menu(void);
-/* Verifies the requested operations validity */
-int valid_operation(int operation);
-
-
-/* Start of the application */
-int main(int argc, char *argv[])
+int main()
 {
-    printf("\n****Calculator****\n");
-    while(1)
-    {
-        calculator_menu();
-    }
-}
-
-void calculator_menu(void)
-{
-    printf("\nAvailable Operations\n");
-    printf("\n1. Add\n2. Subtract\n3. Multiply\n4. Divide\n5. Exit");
-    printf("\n\tEnter your choice\n");
+    customer data[20];
+    int n, z, choice, acc_no, amount, index;
+    char pass[11],password[11]="gaana@2000";
+	login:
+    printf("\n\tEnter the password to login at least 10 characters long:");
+	scanf("%s",pass);
+	z=check(pass,password);
+	if(z)
+	{
+		printf("\nInvalid!");	
+        goto login;		
+      
+	}
+    printf("Banking System\n\n");
+    printf("Number of customer records you want to enter? : ");
+    scanf("%d", &n);
    
-     // __fpurge(stdin);
-    scanf("%d", &calculator_operation);
+    acc(data, n);
+    do
+    {
 
-    if(EXIT == calculator_operation)
-    {
-        printf("\nThank you. Exiting the Application\n");
-        exit(0);
+        printf("\nBanking System Menu :\n");
+        printf("Press 1 to display all records.\n");
+        printf("Press 2 to search a record.\n");
+        printf("Press 3 to deposit amount.\n");
+        printf("Press 4 to withdraw amount.\n");
+        printf("Press 0 to exit\n");
+        printf("\nEnter choice(0-4) : ");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+            case 1:
+                display(data, n);
+                break;
+            case 2:
+                printf("Enter account number to search : ");
+                scanf("%d", &acc_no);
+                index = search(data, n, acc_no);
+                if (index ==  - 1)
+                {
+                    printf("Record not found : ");
+                }
+                else
+                {
+                    printf("A/c Number: %d\nName: %s\nBalance: %d\n",
+                        data[index].acc_no, data[index].name,
+                        data[index].balance);
+                }
+                break;
+            case 3:
+                printf("Enter account number : ");
+                scanf("%d", &acc_no);
+                printf("Enter amount to deposit : ");
+                scanf("%d", &amount);
+                deposit(data, n, acc_no, amount);
+                break;
+           
+            case 4:
+                printf("Enter account number : ");
+                scanf("%d", &acc_no);
+                printf("Enter amount to withdraw : ");
+                scanf("%d", &amount);
+                withdraw(data, n, acc_no, amount);
+        }
     }
-
-    if(INVALID != valid_operation(calculator_operation))
-    {
-        printf("\n\tEnter your Numbers with space between them\n");
-        // __fpurge(stdin);
-        scanf("%d %d", &calculator_operand1, &calculator_operand2);
-    }
-    else
-    {
-        printf("\n\t---Wrong choice---\nEnter to continue\n");
-        // __fpurge(stdin);
-        getchar();
-        return;
-        
-    }
-    switch(calculator_operation)
-    {
-        case ADD:
-            printf("\n\t%d + %d = %d\nEnter to continue", 
-            calculator_operand1, 
-            calculator_operand2,
-            add(calculator_operand1, calculator_operand2));
-            
-            // __fpurge(stdin);
-            getchar();
-            break;
-        case SUBTRACT:
-            printf("\n\t%d - %d = %d\nEnter to continue", 
-            calculator_operand1, 
-            calculator_operand2,
-            subtract(calculator_operand1, calculator_operand2));
-            
-            // __fpurge(stdin);
-            getchar();
-            break;
-        case MULTIPLY:
-            printf("\n\t%d * %d = %d\nEnter to continue", 
-            calculator_operand1, 
-            calculator_operand2,
-            multiply(calculator_operand1, calculator_operand2));
-            
-            // __fpurge(stdin);
-            getchar();
-            break;
-        case DIVIDE:
-            printf("\n\t%d / %d = %d\nEnter to continue", 
-            calculator_operand1, 
-            calculator_operand2,
-            divide(calculator_operand1, calculator_operand2));
-            
-            // __fpurge(stdin);
-            getchar();
-            break;
-        case 5:
-            exit(0);
-            break;
-        default:
-            printf("\n\t---It should never come here---\n");
-    }
-}
-
-int valid_operation(int operation)
-{
-    /* Check if the operation is a valid operation */
-    return ((ADD <= operation) && (EXIT >= operation)) ? VALID: INVALID;
-}
+    while (choice != 0);
+    return 0;
+}     
